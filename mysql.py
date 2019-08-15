0ffaaab813ddab2b04bda83d7885ed7cdd6f41eb
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-08-15 18:34:41 +1200

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,7 +1,7 @@
 #creat and setup mysql
 import MySQLdb
 db = MySQLdb.connect("localhost","root","1stgroup","mysql",charset = "utf8")
-sursor = db.cursor()
+cursor = db.cursor()
 cursor.execute("CREATE DATABASE moodle DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
 cursor.execute("create user 'user1'@'localhost' IDENTIFIED BY '981204';")
 cursor.execute("GRANT ALL ON moodle.* TO user1@localhost IDENTIFIED BY '981204';")
