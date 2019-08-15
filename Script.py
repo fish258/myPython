8405e844bd06991f6306536791cd5f396502aa1d
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-08-15 16:27:01 +1200

    Update Script.py

--- Script.py
+++ Script.py
@@ -34,3 +34,14 @@ fp.close()
 del lines[:]
 os.system("sudo service mysql restart")
 
+#creat and setup mysql
+import MySQLdb
+db = MySQLdb.connect("localhost","root","1stgroup","mysql",charset = "utf8")
+sursor = db.cursor()
+cursor.execute("CREATE DATABASE moodle DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+cursor.execute("create user 'user1'@'localhost' IDENTIFIED BY '981204';")
+cursor.execute("GRANT ALL ON moodle.* TO user1@localhost IDENTIFIED BY '981204';")
+data = cursor.fetchone()
+print(data)
+db.close()
+
