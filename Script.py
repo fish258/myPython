6a732b4f1adf44f4faa1f8ab33c60cf7f56d75a1
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-08-16 16:14:47 +1200

    Update Script.py

--- Script.py
+++ Script.py
@@ -51,10 +51,10 @@ os.system("sudo touch config.php")
 
 #setup the config.php
 import socket,struct,fcntl
-def get_ip(ifname):#get ip address
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', bytes(ifname[:15],'utf-8')))[20:24])
-ownip = get_ip('eth0')
+# def get_ip(ifname):#get ip address
+#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', bytes(ifname[:15],'utf-8')))[20:24])
+# ownip = get_ip('eth0')
 p = os.popen("curl ifconfig.me")
 publicIP = p.read()
 f = open('/var/www/html/moodle/config.php','w')
